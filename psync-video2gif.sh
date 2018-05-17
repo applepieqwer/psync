783b52477c01e53bde819fbd429757c7ed98c874
applepie <applepie@applepie-atom.lan>
2018-05-17 16:14:37 +0800

    video2gif

---
+++ psync-video2gif.sh
@@ -0,0 +1,5 @@
+d=thumbs/
+t=${d}$RANDOM
+ffmpeg -i $1 -vf select='isnan(prev_selected_t)+gte(t-prev_selected_t\,6)' -vsync 2 -vframes 10 ${t}-%03d.png
+convert -delay 100 -loop 0 -quality 95  -resize '200x200^' -gravity center -extent 200x200  ${t}-*.png $2
+rm -f ${t}-*.png
