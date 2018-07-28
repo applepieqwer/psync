c9394f11c15ebc1febd69a5a19c2476cbae46c80
applepieqwer <applepieqwer@gmail.com>
2018-07-28 19:16:44 +0800

    avconv to ffmpeg

--- psync-video2small.sh
+++ psync-video2small.sh
@@ -1,2 +1,2 @@
 rm -f $2
-avconv -i $1 -s:v qvga -c:v libx264 -crf 28 -c:a aac -b:a 128k -preset ultrafast -strict experimental $2
+ffmpeg -i $1 -s:v qvga -c:v libx264 -crf 28 -c:a aac -b:a 128k -preset ultrafast -strict experimental $2
