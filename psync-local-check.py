19eb033ca9852ae9f0c484f9da6bc80feecdef3d
applepieqwer <applepieqwer@gmail.com>
2020-11-01 14:26:54 +0800

    local check args

--- psync-local-check.py
+++ psync-local-check.py
@@ -1,16 +1,18 @@
 ####check fhash with local list
 ####input file is "psync-local-check.input"
 ####download all the fhash with:
-####mysql --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync -ss -e 'SELECT `fhash` FROM `file`;' | sed 's/\t/","/g;s/^//;s/$//;s/\n//g' > psync-local-check.input
-####mysql --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync < sql_log2020-10-18-4192.sql
+####mysql -v --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync -ss -e 'SELECT `fhash` FROM `file`;' | sed 's/\t/","/g;s/^//;s/$//;s/\n//g' > psync-local-check.input
+####mysql -v --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync < sql_log2020-10-18-4192.sql
 
 import __builtin__
 import os
 from ConfigParser import ConfigParser
 from psync_func import ConfigClass,CheckLocal_sql,CheckLocal_wget
 from psync_func import debuglog,debugset,do_sha1,obj2dst
+import getopt
+import sys
 
-def main():
+def main(action):
 	debugset('psync-local-check')
 	cp = ConfigParser()
 	cp.read('psync.conf')
@@ -23,7 +25,10 @@ def main():
 	Config['distserver'] = cp.get('psync_config','distserver')
 	Config['wget_target_url'] = cp.get('psync_local','wget_target_url')
 
-	CheckMan = CheckLocal_wget()
+	if action == 'wget':
+		CheckMan = CheckLocal_wget()
+	if action == 'sql':
+		CheckMan = CheckLocal_sql()
 	with open('psync-local-check.input', 'r') as f:
 		lines = f.readlines()
 		total = len(lines)
@@ -34,7 +39,29 @@ def main():
 
 if __name__ == '__main__':
 	try:
-		main()
+	    options,args = getopt.getopt(sys.argv[1:],'hdu:ws', ['help','download','upload=','wget','sql'])
+	    for name,value in options:
+	    	if name in ('-w','--wget'):
+	    		main('wget')
+	    		sys.exit()
+	    	if name in ('-s','--sql'):
+	    		main('sql')
+	    		sys.exit()
+	    	if name in ('-d','--download'):
+	    		print "Download: mysql -v --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync -ss -e 'SELECT `fhash` FROM `file`;' | sed 's/\\t/\",\"/g;s/^//;s/$//;s/\\n//g' > psync-local-check.input"
+	    		sys.exit()
+	    	if name in ('-u','--upload'):
+	    		print "Upload: mysql -v --compress -upsync -p -hpsync.db.6677333.hostedresource.com psync < %s"%value
+	    		sys.exit()
+	    print "%s usage:"%sys.argv[0]
+	    print "-h --help       : Show this help"
+	    print "-d --download   : Download fhash list from mysql server"
+	    print "-u --upload sql : Upload sql script file to mysql server"
+	    print "-w --wget       : Check fhash and output wget script"
+	    print "-s --sql        : Check fhash and output sql script"
+	    sys.exit()
+	except getopt.GetoptError:
+	    sys.exit()
 	except KeyboardInterrupt:
 		print 'KeyboardInterrupt'
-		exit()
+		sys.exit()
